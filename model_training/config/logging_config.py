baced458202e436055af5e7ef3f4c3a67c6c2da9
Ludvig Szklarz Anderson <ludvig.s.anderson@gmail.com>
2025-08-04 20:58:13 +0200

    Fixed logging

--- model_training/config/logging_config.py
+++ model_training/config/logging_config.py
@@ -1,7 +1,9 @@
+import sys
 import logging
 
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        stream=sys.stdout,
     )
--- model_training/main.py
+++ model_training/main.py
@@ -2,11 +2,10 @@ from training import train_aft_model, train_causal_model
 import logging
 from config.logging_config import setup_logging
 
-logger = logging.getLogger(__name__)
-
 def main():
-    logger.info("FUNCTION main HAS STARTED.")
     setup_logging()
+    logger = logging.getLogger(__name__)
+    logger.info("FUNCTION main HAS STARTED.")
     train_aft_model()
     train_causal_model()
 
