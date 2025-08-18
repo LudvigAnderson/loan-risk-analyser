f78fd2005a29a2ccc754587204b7f9a07f31eea8
Ludvig Szklarz Anderson <ludvig.s.anderson@gmail.com>
2025-08-18 05:09:19 +0200

    Made divisions safe

--- backend/app/services/pipeline.py
+++ backend/app/services/pipeline.py
@@ -7,28 +7,44 @@ from xgboost import Booster
 
 from pandas import DataFrame
 
+from pandas import Series
+
+def divide(s1: Series, s2: Series) -> Series:
+    s2_nonzero = s2 != 0
+
+    a = s1.to_numpy(dtype=float, copy=False)
+    b = s2.to_numpy(dtype=float, copy=False)
+
+    out = np.full_like(a, np.nan)
+    np.divide(a, b, out=out, where=s2_nonzero)
+
+    return pd.Series(out, index=s1.index)
+
+def dividep1(s1: Series, s2: Series) -> Series:
+    return divide(s1, s2 + 1)
+
+
+
 def transform_data(data: LoanApplicant, app: FastAPI) -> DataFrame:
     df = pd.DataFrame([data.model_dump()])
     df = df.replace({None: np.nan})
 
-    # Need to use the safe division
-
-    df["bc_util"] = df["bc_bal"] / df["total_bc_limit"]
-    df["dti"] = df["monthly_installments"] / (df["annual_inc"] / 12)
+    df["bc_util"] = divide(df["bc_bal"], df["total_bc_limit"])
+    df["dti"] = divide(df["monthly_installments"], (df["annual_inc"] / 12))
 
     init_il = df["init_mortgage"] + df["init_student_loan"] + df["init_car_loan"] + df["init_consumer_loan"]
     cur_il = df["mortgage"] + df["student_loan"] + df["car_loan"] + df["consumer_loan"]
-    df["il_util"] = cur_il / init_il
+    df["il_util"] = divide(cur_il, init_il)
 
     df["tot_cur_bal"] =  df["bc_bal"] + cur_il
     df["total_bal_ex_mort"] = df["tot_cur_bal"] - df["mortgage"]
 
     df["percent_bc_gt_75"] = df["percent_bc_gt_75"] / 100
 
-    df["monthly_principal"] = df["loan_amnt"] / df["term"]
-    df["principal/inc"] = df["monthly_principal"] / df["annual_inc"]
-    df["total_bal/inc"] = df["tot_cur_bal"] / df["annual_inc"]
-    df["acc_satisfied_rate"] = np.divide(df["num_sats"], df["total_acc"])
+    df["monthly_principal"] = divide(df["loan_amnt"], df["term"])
+    df["principal/inc"] = dividep1(df["monthly_principal"], df["annual_inc"])
+    df["total_bal/inc"] = dividep1(df["tot_cur_bal"], df["annual_inc"])
+    df["acc_satisfied_rate"] = divide(df["num_sats"], df["total_acc"])
 
     # In a real production environment, I would consider setting
     # this value based on an API call to Statistics Norway, so that
