4eb8797b1298731dbe024b7eb932374c750ca655
Ludvig Szklarz Anderson <ludvig.s.anderson@gmail.com>
2025-08-24 02:58:52 +0200

    Made a short introduction header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,13 @@ function App() {
       </header>
       <main className="container mx-auto px-4 pt-4">
         <section>
+          <div className="pb-10">
+            <h1 className="header1">Loan Risk Analyser</h1>
+            <p>Enter loan applicant information manually or use the preset buttons.</p>
+            <p>Click the <strong>Predict risk</strong> button to see predictions of default risk and the recommended interest rate.</p>
+          </div>
+        </section>
+        <section>
           <DataProvider>
             <FormProvider>
               <div className="border-b mb-2 w-full items-center"><h2 className="header2 text-center">Loan & Applicant Information</h2></div>
